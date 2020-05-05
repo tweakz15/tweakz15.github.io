e192cf168918fbfd8da03e4b4b52487d141595a7
tweakz15 <tweakz1502@gmail.com>
2020-05-06 04:09:39 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function realClock(){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime (m);
+    s = checkTime (s);
+
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(realClock, 500);
+}
+
+function checkTime(i){
+    if(i < 10) {i = '0' + 1};
+    return i;
+}
